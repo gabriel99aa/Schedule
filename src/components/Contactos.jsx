580cceeb04ff0d484f3b64ca1eb4c155f39c0395
Gabriel <gabriel.agudelo@utp.edu.co>
2022-04-12 15:04:52 -0500

    local Storage

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -1,28 +1,20 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { ContactosReducer } from "../reducers/ContactosReducer";
 import TablaContactos from "./TablaContactos";
 import FormularioAdd from "./FormularioAdd";
 
-const contactos = [
-  {
-    id: "asbc",
-    nombre: "gabriel",
-    numero: "2384791235",
-  },
-  {
-    id: "asbcsdfa",
-    nombre: "esteban",
-    numero: "94567891235",
-  },
-  {
-    id: "asdgdfhw5yfa",
-    nombre: "agudelo",
-    numero: "945691235",
-  },
-];
+const third = () => {
+  const contactos = localStorage.getItem("contactos");
+
+  return contactos ? JSON.parse(contactos) : [];
+};
 
 const Contactos = () => {
-  const [state, dispatch] = useReducer(ContactosReducer, contactos);
+  const [state, dispatch] = useReducer(ContactosReducer, [], third);
+
+  useEffect(() => {
+    localStorage.setItem("contactos", JSON.stringify(state));
+  }, [state]);
 
   return (
     <div className="container mt-3">
