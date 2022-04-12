7fb44dd784e8ce8b044119e29ea3432c04670dc6
Gabriel <gabriel.agudelo@utp.edu.co>
2022-04-11 20:45:21 -0500

    Primer Reducer

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { ContactosReducer } from "../reducers/ContactosReducer";
 import TablaContactos from "./TablaContactos";
+import FormularioAdd from "./FormularioAdd";
+
+const contactos = [
+  {
+    id: "asbc",
+    nombre: "gabriel",
+    numero: "2384791235",
+  },
+  {
+    id: "asbcsdfa",
+    nombre: "esteban",
+    numero: "94567891235",
+  },
+  {
+    id: "asdgdfhw5yfa",
+    nombre: "agudelo",
+    numero: "945691235",
+  },
+];
 
 const Contactos = () => {
-  const contactos = [
-    {
-      id: "asbc",
-      nombre: "gabriel",
-      numero: "2384791235",
-    },
-    {
-      id: "asbcsdfa",
-      nombre: "esteban",
-      numero: "94567891235",
-    },
-  ];
+  const [state, dispatch] = useReducer(ContactosReducer, contactos);
+
   return (
     <div className="container mt-3">
-      <TablaContactos contactos={contactos} />
+      <FormularioAdd />
+      <TablaContactos contactos={state} />
     </div>
   );
 };
---
+++ src/components/FormularioAdd.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FormularioAdd = () => {
+  return (
+    <div className="container">
+      <label className="mx-1 d-grid gap-2">
+        Nombre:{" "}
+        <input type="text" className="form-control" autoComplete="off" />
+      </label>
+      <label className="mx-1 d-grid gap-2">
+        Número:{" "}
+        <input type="text" className="form-control" autoComplete="off" />
+      </label>
+      <div className="mx-1 d-grid gap-2">
+        <button className="btn btn-info mt-3">Agregar</button>
+      </div>
+    </div>
+  );
+};
+
+export default FormularioAdd;
